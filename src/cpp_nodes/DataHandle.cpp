2f7cdfb5fb15a863ab9ebb0b780b03687d7143e4
Mikhail Hyde <mhyde64@my.uri.edu>
2019-11-22 09:10:07 -0500

    Built C++ coordinatePass client

--- src/cpp_nodes/DataHandle.cpp
+++ src/cpp_nodes/DataHandle.cpp
@@ -1,59 +1,29 @@
-#include <iostream>
-#include <math.h>
+#include "ros/ros.h"
+#include "motor_control/coordinatePass.h"
+#include <cstdlib>
 
-#include "DataHandle.h"
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "coordinatePassClient");
+  if (argc != 3)
+  {
+    ROS_INFO("Usage: Receiving X Y Z coordinate ... ");
+    return 1;
+  }
 
-using namespace std;
+  ros::NodeHandle node;
+  ros::ServiceClient client = node.serviceClient<motor_control::coordinatePass>("coordinatePass");
+  motor_control::coordinatePass msg;
+  msg.request = true;
+  if (client.call(msg))
+  {
+    ROS_INFO("%ld", (long int)msg.response.xCoordinate, (long int)msg.response.yCoordinate, (long int)msg.response.zCoordinate);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service add_two_ints");
+    return 1;
+  }
 
-DataHandle::DataHandle(float xComp, float yComp, float zComp){
-    this->xComp = xComp;
-    this->yComp = yComp;
-    this->zComp = zComp;
-}
-
-void DataHandle::positionVectorDef(float *length, float *pvAng){
-    float xPrime = sqrt(pow(xComp, 2) + pow(yComp, 2));
-    *pvAng = (atan(zComp / xPrime)) * 180 / PI;
-    *length = sqrt(pow(xPrime, 2) + pow(zComp, 2));
-}
-
-bool DataHandle::positionVectorCheck(){
-    float comparisonValue = sqrt(pow(xComp, 2) + pow(yComp, 2) + pow(zComp, 2));
-    return 2 <= comparisonValue <= 10;
-    }
-
-float DataHandle::baseAngle(){
-    float baseAng;
-    if (yComp > 0){
-        baseAng = (atan2(yComp, xComp)) * 180 / PI;
-    }
-    else if (yComp < 0){
-        baseAng = ((atan2(yComp, xComp)) * 180 / PI) + 360;
-    }
-    else if (xComp == 0 && yComp > 0){
-        baseAng = 90;
-    }
-    else if (xComp == 0 && yComp < 0){
-        baseAng = 270;
-    }
-    else if (xComp > 0 && yComp == 0){
-        baseAng = 0;
-    }
-    else if (xComp < 0 && yComp == 0){
-        baseAng = 180;
-    }
-
-    return baseAng;
-}
-
-void DataHandle::armAngles(float *mainDegFromZ, float *secAng, float *toolAng){
-
-}
-
-void DataHandle::genAngles(float *baseAng, float *mainAng, float *secAng, float *toolAng){
-
-}
-
-DataHandle::~DataHandle(void){
-    cout << "Destroying Class ... " << endl;
+  return 0;
 }
--- src/cpp_nodes/DataHandle.h
+++ src/cpp_nodes/DataHandle.h
@@ -3,16 +3,11 @@
 
 class DataHandle{
 private:
-    float xComp, yComp, zComp;
-    float PI = 3.1415;
+    
 public:
     DataHandle(float xComp, float yComp, float zComp);
 
-    void positionVectorDef(float *length, float *pvAng);
-    bool positionVectorCheck();
-    float baseAngle();
-    void armAngles(float *mainDegFromZ, float *secAng, float *toolAng);
-    void genAngles(float *baseAng, float *mainAng, float *secAng, float *toolAng);
+
     
     ~DataHandle(void);
 };
