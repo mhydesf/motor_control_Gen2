408c981da2a24a151325864d9f0b60e8997401f7
Mikhail Hyde <hyde.mikhail@gmail.com>
2019-12-21 18:36:35 -0500

    Clean Merge to Master

---
+++ src/arduino/.build/uno/ros_lib/dependencies.d
@@ -0,0 +1,3 @@
+.build/uno/ros_lib/time.d .build/uno/ros_lib/time.o: lib/ros_lib/time.cpp lib/ros_lib/ros/time.h \
+ lib/ros_lib/ros/duration.h
+.build/uno/ros_lib/duration.d .build/uno/ros_lib/duration.o: lib/ros_lib/duration.cpp lib/ros_lib/ros/duration.h
--- src/arduino/.build/uno/src/arduino/arduino.d
+++ src/arduino/.build/uno/src/arduino/arduino.d
@@ -19,14 +19,3 @@
  lib/ros_lib/ArduinoHardware.h \
  /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
  lib/ros_lib/motor_control/motorSteps.h
-.build/uno/src/arduino.d .build/uno/src/arduino.o: .build/uno/src/arduino.cpp \
- /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
- /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h
